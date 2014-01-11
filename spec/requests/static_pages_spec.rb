5f1e01cdf2b4756cd546de217dac715e6649d159
Koen Klasing <koen@bureaublauwgeel.nl>
2014-01-11 11:47:56 +0100

    Updated contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
+
 describe "StaticPages" do
+
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
   describe "Home page" do
     
@@ -11,8 +14,7 @@ describe "StaticPages" do
 
     it "should have the right title" do
       visit '/static_pages/home'
-      page.should have_selector('title', 
-        :text => 'Ruby on Rails Tutorial Sample App | Home')
+      page.should have_selector('title', :text => "#{base_title} | Home")
     end
 
   end
@@ -27,7 +29,7 @@ describe "StaticPages" do
     it "should have the right title" do
       visit '/static_pages/help'
       page.should have_selector('title', 
-        :text => 'Ruby on Rails Tutorial Sample App | Help')
+        :text => "#{base_title} | Help")
     end
 
   end
@@ -42,7 +44,7 @@ describe "StaticPages" do
     it "should have the right title" do
       visit '/static_pages/about'
       page.should have_selector('title', 
-        :text => 'Ruby on Rails Tutorial Sample App | About Us')
+        :text => "#{base_title} | About Us")
     end
 
   end
@@ -57,7 +59,7 @@ describe "StaticPages" do
     it "should have the right title" do
       visit '/static_pages/contact'
       page.should have_selector('title', 
-        :text => 'Ruby on Rails Tutorial Sample App | Contact')
+        :text => "#{base_title} | Contact")
     end
 
   end
